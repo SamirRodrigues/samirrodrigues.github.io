713c6154d82b60bb8a6e3409a5c6e761c7c04cca
Jorge Confessor de Moura Junior <jorgeconfessor@yahoo.com>
2024-10-08 10:16:31 -0300

    Update LaunchSection.js

--- src/components/inicio/LaunchSection.js
+++ src/components/inicio/LaunchSection.js
@@ -2,16 +2,23 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { FaChevronRight } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import "./LaunchSection.css";
 
 const LaunchSection = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate("/jogos");
+    window.scrollTo(0, 0); // Rola a página para o topo
+  };
+
   return (
     <section className="launch-section">
       <Container>
         <Row className="justify-content-center">
           <Col lg={10} className="px-0">
             {/* Logo exibida apenas no mobile, acima do conteúdo */}
-
             <h2 className="launch-title">
               Conheça nosso <strong>lançamento!</strong>
             </h2>
@@ -25,7 +32,12 @@ const LaunchSection = () => {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
-                <Button variant="dark" className="launch-btn">
+                {/* Botão que redireciona para a página de jogos */}
+                <Button
+                  variant="dark"
+                  className="launch-btn"
+                  onClick={handleNavigation}
+                >
                   <FaChevronRight />
                   Ver mais
                 </Button>
