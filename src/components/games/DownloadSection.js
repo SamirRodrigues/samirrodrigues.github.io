5a4d766a7819c6837a4186ef106cd96c7621a5bc
Jorge Confessor de Moura Junior <jorgeconfessor@yahoo.com>
2025-02-06 14:35:28 -0300

    update link for download

--- src/components/games/DownloadSection.js
+++ src/components/games/DownloadSection.js
@@ -4,21 +4,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./DownloadSection.css";
 
 const downloads = [
-  {
-    id: 1,
-    title: "Acesse nossas pesquisas",
-    description:
-      "Explore estudos sobre a cultura quilombola e sua resistência histórica, preservando memórias e saberes.",
-    image: "assets/download1.png",
-    link: "#",
-  },
+  // {
+  //   id: 1,
+  //   title: "Acesse nossas pesquisas",
+  //   description:
+  //     "Explore estudos sobre a cultura quilombola e sua resistência histórica, preservando memórias e saberes.",
+  //   image: "assets/download1.png",
+  //   link: "/assets/pesquisas_quilombolas.pdf", // Exemplo de outro documento
+  // },
   {
     id: 2,
     title: "Baixe nosso plano de aulas",
     description:
       "Tenha acesso aos materiais didáticos para ensinar a história dos quilombos de forma dinâmica e inspiradora.",
     image: "assets/download2.png",
-    link: "#",
+    link: "/assets/Planos%20de%20Aula%20-%20BANZ%C3%89.pdf", // Caminho atualizado para o PDF
   },
 ];
 
@@ -36,7 +36,7 @@ const DownloadSection = () => {
                 <div className="download-content text-center">
                   <h3>{download.title}</h3>
                   <p>{download.description}</p>
-                  <a href={download.link} className="download-button">
+                  <a href={download.link} className="download-button" download>
                     <i className="fas fa-download"></i> Fazer download
                   </a>
                 </div>
